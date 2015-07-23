c7289ab0c0e158769deca3f479f8b3757b5de110
jaqqz <pnieters@uos.de>
2015-07-23 15:13:43 +0200

    testing1

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -1,13 +1,38 @@
 # base program: MANAGER
 
-from vision import see
+from vision.see import See
+from language.hear import Hear
+import response.response
 
 class Sherlock:
 
     # DUMMY
     def __init__(self, string):
-        self.name = string;
+        noSessions = 0
+        activeSession = False
+        self.name = string
 
-    def visionTags(imagePath):
+    def startSession(self):
+        noInteraction = 0
+        sessionID = noSession + 1
+        activeSession = True
+
+    def endSession(self):
+        activeSession = False
+
+    def process(self, input):
+        if not activeSession:
+            startSession()
+            response['text'] = proc_UserInp('',noInteraction)
+            response['imagePATH'] = 'a140032.jpg'
+
+            return response
+        else:
+            noInteraction += 1
+            reponse['text'] = proc_UserImp(input, noInteraction)
+
+            return response
+
+    def visionTags(self, imagePath):
         tags = see.getTags(imagePath)
         return tags
--- test_script.py
+++ test_script.py
@@ -1,5 +1,12 @@
-from sherlock.vision.see import See
+from sherlock.sherlock import Sherlock
 
-see = See()
-res = see.getTags('a140032.jpg')
-print(res)
+handler = Sherlock('handler')
+
+resp1 = handler.process('')
+print(resp1['text'])
+
+resp2 = handler.process('There is a house')
+print(resp2['text'])
+
+resp3 = handler.process('There is also a frenchman')
+print(resp3['text'])
