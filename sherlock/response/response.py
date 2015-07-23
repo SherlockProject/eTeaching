a681829a043678e2e8b571422d5b76e3d1b865de
jaqqz <pnieters@uos.de>
2015-07-23 15:31:09 +0200

    classnow

--- sherlock/response/response.py
+++ sherlock/response/response.py
@@ -1,16 +1,15 @@
-def proc_userInp(sentence, num_interaction):
-	if num_interaction == 0:
-		return "Hello, I'm Sherlock, who are you?"
-	elif num_interaction == 1:
-		return "What does the picture in the left show?"
-	elif num_interaction > 2:
-		rand = random.randint(1, 3)
-		if rand == 1:
-			return "Could you try to be more specific?"
-		elif rand == 2:
-			return "Could you try to be more general?"
-		elif rand == 3: # If answer does not contain annotation
-			return "Can you also see (tag 1)?"
-				  
-		  
-	 
+class Answer:
+
+	def rndmReply(sentence, num_interaction):
+		if num_interaction == 0:
+			return "Hello, I'm Sherlock, who are you?"
+		elif num_interaction == 1:
+			return "What does the picture in the left show?"
+		elif num_interaction > 2:
+			rand = random.randint(1, 3)
+			if rand == 1:
+				return "Could you try to be more specific?"
+			elif rand == 2:
+				return "Could you try to be more general?"
+			elif rand == 3: # If answer does not contain annotation
+				return "Can you also see (tag 1)?"
