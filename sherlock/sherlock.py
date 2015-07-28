86b917cfdbfaa09a62e2ac26779737930e983f15
jaqqz <pnieters@uos.de>
2015-07-28 14:45:30 +0200

    in_Progress

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -1,40 +1,97 @@
 # base program: MANAGER
 
-from sherlock.vision.see import See
-from sherlock.language.hear import Hear
-from sherlock.response.response import proc_userInp
+from sherlock.vision.visanalyser import VisAnalyser
+from sherlock.language.langanalyser import LangAnalyser
+from sherlock.response.dialoguehandler import DialogueHandler
 
 class Sherlock:
 
-    # DUMMY
+    # Initialise clean Instance.
     def __init__(self, string):
         self.noSessions = 0
         self.activeSession = False
-        self.name = string
+        self.currImage = ''
 
-    def startSession(self):
+        # Bind and instantiate all analysers
+        self.Vision = VisAnalyser()
+        self.Language = LangAnalyser()
+        self.Dialoge = DialogueHandler()
+
+    # public access to processing in the sherlock project!
+    # will return a dict with return type, message and possibly
+    # the path to an image. See specification of Frontend-Backend
+    # communication.
+    def process(self, request):
+
+        if request['type'] == 'start'
+            respMsg = self.__startSession()
+            response = {'type': 'image',
+                        'img_path': self.currImage,
+                        'text': respMsg}
+            return response
+
+        elif request['type'] == 'message'
+            if self.activeSession = False:
+                return response = {'type': 'message',
+                                   'text': 'ERROR: No active Session'}
+            # gather all analytics in one object.
+            analysis = self.__analyse(request['text'], self.currImage)
+
+            # based on the analysis, compute response and return.
+            return self.__respond(analysis)
+
+        else
+            response = {'type': 'message',
+                        'text': 'ERROR: Unknown Request Type'}
+
+    # Analyse a std message and store information!
+    # 1st Iteration - SIMPLE
+    # private.
+    def __analyse(self, text, imagePATH):
+        # get list of image tags
+        tagList = self.Vision.getTags(imagePATH)
+
+        # get Relations, Trees, ..
+        lingAnalysis = self.Language.analyse(text)
+
+        # combine, clean up
+        pass
+
+        # store
+        pass
+
+        analysis = {'tags': tagList,
+                    'linguistics': lingAnalysis}
+        # return
+        return analysis
+
+
+    # Computes response based on analysis.
+    # Will return a comlete response dict.
+    # private.
+    def __respond(self, analysis):
+
+        resonse = self.Dialogue.respond(analysis)
+        # housekeeping: new Image? compose proper response dict etc...
+        pass
+        return response
+
+    # Start a session:
+    # Reset Interactions; Set SessionID;
+    # Flag active Session; Get 'Hello' Message
+    # private.
+    def __startSession(self):
         self.noInteraction = 0
         self.sessionID = self.noSessions + 1
         self.activeSession = True
 
-    def endSession(self):
+        # Set first Image;
+        self.currImage = 'a140032.jpg'
+        return 'Hello, I am Sherlock! Please describe the picture on the left!'
+
+    # End a session:
+    # Reset flags!
+    # private.
+    def __endSession(self):
+        self.currImage =  ''
         self.activeSession = False
-
-    def process(self, input):
-        if not self.activeSession:
-            self.startSession()
-            response = {}
-            response['text'] = proc_userInp('',self.noInteraction)
-            response['imagePATH'] = 'a140032.jpg'
-
-            return response
-        else:
-            self.noInteraction += 1
-            response = {}
-            response['text'] = proc_userInp(input, self.noInteraction)
-
-            return response
-
-    def visionTags(self, imagePath):
-        tags = see.getTags(imagePath)
-        return tags
